e2dc2a66efdd67bade845a340923076b92a99503
Sebastian Flores <sebastianflores@ihfintech.com.pe>
2025-04-09 02:36:35 -0500

    feat: first commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clean any existing build artifacts
+rm -f bootstrap
+
+# Build the Go binary for Linux
+GOOS=linux GOARCH=amd64 go build -o bootstrap
+
+# Make the binary executable
+chmod +x bootstrap
